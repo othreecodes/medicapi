6dde0aa38565c340e25de836a473a9b18d0ac536
Obi Uchenna David <daviduchenna@outlook.com>
2018-01-01 13:04:25 +0100

    done with heroku config

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name','last_name','username','email','password')
+        fields = ('first_name','last_name','username','email','password') + ('is_staff','is_active','date_joined')
+        read_only_fields = ('is_staff','is_active','date_joined')
